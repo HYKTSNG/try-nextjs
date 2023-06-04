c6f86c8cc53797792279d9f6716789bb837ad1c2
HYKTSNG <Sin5hayak@gmail.com>
2023-06-04 15:33:45 +0900

    css追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,19 @@
+import Link from "next/link";
+
 export default function Home() {
-	return (
-		<>
-			<section>
-        <h1>
-          Home
-				</h1>
+  return (
+    <>
+      <section>
+        <h1>Home</h1>
         <p>リンク集</p>
-			</section>
-		</>
-	);
+        <ul>
+          <li>
+            項目1: <Link href={"/posts"}>posts page!</Link>
+          </li>
+          <li>項目2</li>
+          <li>項目3</li>
+        </ul>
+      </section>
+    </>
+  );
 }
--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,6 +1,24 @@
-import type { FC } from 'react';
+import Link from "next/link";
+import type { FC } from "react";
+import "../styles/global.css";
 
 const FirstPost: FC = () => {
-	return <h1>First Post</h1>;
+  // return <h1>First Post</h1>;
+  return (
+    <section>
+      <link
+        href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        rel="stylesheet"
+      ></link>
+      <h1>First Post</h1>
+      <h2 className="back">
+        <Link href={"/"}>
+          <span className="material-icons test">arrow_right_alt</span>
+          Back to home
+        </Link>
+      </h2>
+    </section>
+  );
 };
+
 export default FirstPost;
